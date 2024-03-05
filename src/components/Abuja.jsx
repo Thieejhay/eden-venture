6e058b9ef4f33df0fc8a4307e094f0019396c8bb
Tijani Tofunmi <tijani.tofunmi@gmail.com>
2024-03-05 19:33:19 +0100

    new commits

--- src/components/Abuja.jsx
+++ src/components/Abuja.jsx
@@ -136,7 +136,7 @@ const Abuja = () => {
               
           </div>
         </div>
-        <div className='flex justify-center items-center mb-6'><Link to='/register'><span className='flex items-center lg:px-12 px-3 lg:py-5 py-3 border-none rounded-md bg-[#6100c1]/70 w-fit text-3xl text-[#E2E7E9]/90 uppercase'>Register</span></Link></div>
+        <div className='flex justify-center items-center mb-6'><Link to='/register'><span className='flex items-center lg:px-12 px-3 lg:py-5 py-2 border-none rounded-md bg-[#6100c1]/70 w-fit text-3xl text-[#E2E7E9]/90 uppercase'>Register</span></Link></div>
       </div>
       <div className='flex flex-col lg:flex-row gap-6 lg:gap-36 bg-[#6100c1] px-4 py-10 lg:justify-center lg:items-center'>
         <span className='flex flex-col gap-2'>
